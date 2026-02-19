bb56a69de6c42bad84aa0de4ffe7677241bfcc1d
naveenjavvadi05-maker <naveenjavvadi05@gmail.com>
2026-02-19 10:06:42 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'learnhub-v1';
+const urlsToCache = ['/','index.html','styles.css','app.js','db.js','games.js','utils.js','data.json'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
